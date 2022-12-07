f4b81701a639f0a496a545dd776cb27554c7fd1d
Satyendra <satyendrasinghbca777@gmail.com>
2022-12-07 23:42:50 +0530

    we are create directory and in a test file

--- php/test.php
+++ php/test.php
@@ -1,4 +1,13 @@
 <?php
 echo "Hello This is the first program to commit on github";
 echo date('Y-m-d');
+
+/* HOW TO USE CURL IN PHP */
+$url_name = "https://google.com";
+$ch_session = curl_init();
+curl_setopt($ch_session, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch_session, CURLOPT_URL, $url);
+$result_url = curl_exec($ch_session);
+echo $result_url;
+
 ?>
